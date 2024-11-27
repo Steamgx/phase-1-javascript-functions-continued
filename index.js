4eec08fe30c02d92998389403378ac8ba5babe3b
Steamgx <your-bmuraya646@gmail.com>
2024-11-27 14:17:29 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  // Function definitions in your main file (e.g., index.js)
+
+const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  const wrapAdjective = function(wrapper = '*') {
+    return function(adjective) {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+  };
+  
+  // Export the functions to make them accessible in your test files
+
+  
